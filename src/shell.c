97a43a2969daff3dd6b7571f7fb0ae0bce493cc8
Jason Dan <jason.dan@mail.mcgill.ca>
2021-02-26 12:10:52 -0500

    not quite sure, v2

--- src/shell.c
+++ src/shell.c
@@ -1,6 +1,6 @@
 /****************************************************
- * shell.c - This is the primary programming file.  *
- * It contains the main() and parse() functions.    *
+ * shell.c - This is the primary UI program file.   *
+ * It contains the shellUI() functions.             *
  * Jason Dan | 260718739                            *
  ****************************************************/ 
 
@@ -12,16 +12,10 @@
 #include "shellmemory.h"
 #include "shell.h"
 
-void cleanup();
-
-int main(int argc, char *argv[]) {
-    atexit(cleanup);
-    SM_init();
-    
-    const char *welcome = "Welcome to the Jason shell!\nVersion 1.0 Created January 2020\n";
+int shellUI() {
+    const char *welcome = "Welcome to the Jason shell!\nVersion 2.0 Updated February 2021\n";
     const char *prompt = "$";
 
-    int errorCode = EXIT_SUCCESS;
     char *inputBuffer = (char *) malloc(sizeof(char) * USER_INPUT_BUFFER_SIZE);
 
     printf("%s", welcome);
@@ -32,53 +26,23 @@ int main(int argc, char *argv[]) {
         fgets(inputBuffer, (USER_INPUT_BUFFER_SIZE - 1), stdin);
 
         char** words = parseInput(inputBuffer);
-        errorCode = interpreter(words);
+        int errorCode = interpreter(words);
         cleanupWords(words);
 
         if (errorCode == -1) {
-            free(inputBuffer);
-            printf("Bye!\n");
-            exit(EXIT_SUCCESS);
+            break;
         }
+
         displayError(errorCode);
     }
+
+    free(inputBuffer);
+    printf("Bye!\n");
+    return EXIT_SUCCESS;
 }
 
-char** parseInput(char ui[]) {
-    char tmp[200];
-    char **words = (char**) malloc(sizeof(char*) * MAX_WORD_COUNT);
-    int a,b;      // Char index in ui and word
-    int w = 0;  // Word counter
 
-    for (a = 0; ui[a] == ' ' && a < USER_INPUT_BUFFER_SIZE; a++);  // Skip spaces
 
-    while (ui[a] != '\0' && a < USER_INPUT_BUFFER_SIZE) {
-        for (b = 0; ui[a] != '\0' && ui[a]!= ' ' && ui[a]!= '\n' && a < USER_INPUT_BUFFER_SIZE; a++, b++) {
-            tmp[b] = ui[a];
-        }
-        tmp[b] = '\0';
-        words[w] = strdup(tmp);
-
-        while ((ui[a] == ' ' || ui[a] == '\n') && a < USER_INPUT_BUFFER_SIZE) a++;
-        w++;
-    }
-
-    return words;
-}
-
-// Frees the memory of an array of words
-void cleanupWords(char** words) {
-    for (int i = 0; i < MAX_WORD_COUNT && words[i]; i++) {
-        free(words[i]);
-        words[i] = NULL;
-    }
-    free(words); 
-    words = NULL;
-}
-
-void cleanup() {
-    SM_cleanup();
-}
 
 // Utility method to display error messages.
 void displayError(int error) {
@@ -94,14 +58,3 @@ void displayError(int error) {
     }
 }
 
-// Utility method to duplicate strings like POSIX strdup, minus the GCC errors.
-// Source: https://cboard.cprogramming.com/c-programming/95462-compiler-error-warning-implicit-declaration-function-strdup.html
-char *strdup(const char *str)
-{
-    int n = strlen(str) + 1;
-    char *dup = malloc(n);
-    if(dup) {
-        strcpy(dup, str);
-    }
-    return dup;
-}
