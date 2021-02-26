2f29611ce4eaee0b8e7316d145455b3204886e7d
Jason Dan <jason.dan@mail.mcgill.ca>
2021-02-26 13:58:52 -0500

    refactor strdup to util file

--- src/shell.c
+++ src/shell.c
@@ -11,6 +11,7 @@
 #include "interpreter.h"
 #include "shellmemory.h"
 #include "shell.h"
+#include "util.h"
 
 void cleanup();
 
@@ -94,14 +95,3 @@ void displayError(int error) {
     }
 }
 
-// Utility method to duplicate strings like POSIX strdup, minus the GCC errors.
-// Source: https://cboard.cprogramming.com/c-programming/95462-compiler-error-warning-implicit-declaration-function-strdup.html
-char *strdup(const char *str)
-{
-    int n = strlen(str) + 1;
-    char *dup = malloc(n);
-    if(dup) {
-        strcpy(dup, str);
-    }
-    return dup;
-}
