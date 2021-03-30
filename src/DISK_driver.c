f8df75da16165888d24f383379a7bed3c7282878
Jason Dan <jasondan1234@gmail.com>
2021-03-30 01:55:27 -0400

    Implemented partition()

--- src/DISK_driver.c
+++ src/DISK_driver.c
@@ -18,6 +18,9 @@ char* getPartitionPath(char *name);
 // Filepath to partition folder
 const char partitionPath[] = "./PARTITION/";
 
+// Partition file extension
+const char partitionExt[] = ".partition";
+
 // Local copy of active partition metadata
 struct PARTITION {
     unsigned char total_blocks; // Total blocks for file data
@@ -27,11 +30,11 @@ struct PARTITION {
 
 // Local copy of active FAT
 struct FAT {
-    char *filename;             // Limit is 20 chars (including null end character)
-    unsigned char file_length;  // Number of data blocks affiliated with the file
-    unsigned char blockPtrs[10];// Pointers to the blocks in the parttion populated with data. These pointers are block numbers.
+    char filename[20];              // Limit is 20 chars (including null end character)
+    unsigned char file_length;      // Number of data blocks affiliated with the file
+    unsigned char blockPtrs[10];    // Pointers to the blocks in the parttion populated with data. These pointers are block numbers.
     signed char current_location;   // Pointer to the current read/write location of the file in block number. Initialized to -1 when not used.
-} localFAT;
+} localFAT[20];
 
 // Block buffer is malloced to the correct size 
 // when the partition() function is called, with the size
@@ -53,6 +56,26 @@ int active_file_map[5];
 
 void initIO() {
 
+    // Initialize local partition data
+    localPartition.block_size = 0;
+    localPartition.total_blocks = 0;
+    localPartition.used_blocks = 0;
+
+    // Initialize all 20 entries of local FAT
+    for (int i = 0; i < 20; i++) {
+
+        // Fill filename with null
+        memset(localFAT[i].filename, '\0', sizeof(localFAT[i].filename));
+
+        localFAT[i].file_length = 0;
+
+        for (int j = 0; j < 10; j++) {
+            localFAT[i].blockPtrs[j] = 0;
+        }
+        
+        localFAT[i].current_location = -1;
+    }
+
     // Initialize active file map
     for (int i = 0; i < 5; i++) {
         active_file_map[i] = -1;
@@ -62,6 +85,15 @@ void initIO() {
 
 int partition(char *name, int blocksize, int totalblocks) {
 
+    int errorCode = EXIT_SUCCESS;
+
+    // Reset local partition and FAT structures
+    initIO();
+
+    // Populate local partition struct with params
+    localPartition.block_size = (unsigned int) blocksize;
+    localPartition.total_blocks = (unsigned char) totalblocks;
+
     // Create PARTITION folder if not present.
     if (access(partitionPath, F_OK)) {
         char command[100];
@@ -70,7 +102,39 @@ int partition(char *name, int blocksize, int totalblocks) {
         system(command);
     }
 
-    
+    // Create partition file
+    char *filepath = getPartitionPath(name);
+    FILE* pfile = fopen(filepath, "wb");
+    free(filepath);
+    if (!pfile) return EXIT_FAILURE;
+
+    // Write partition to file
+    if (fwrite(&localPartition, sizeof(localPartition), 1, pfile) != 1) errorCode = EXIT_FAILURE;
+
+    // Write FAT to file
+    if (fwrite(&localFAT, sizeof(localFAT), 1, pfile) != 1) errorCode = EXIT_FAILURE;
+
+    // Write (blocksize * total blocks) '0' to file.
+    for (int i = 0; i < (blocksize * totalblocks); i++) {
+        fputc('0', pfile);
+    }
+
+    fclose(pfile);
+
+    // If file write was unsuccessful, then delete file and return failure.
+    if (errorCode) {
+        filepath = getPartitionPath(name);
+        char command[100];
+        strcpy(command, "rm -f ");
+        strcat(command, filepath);
+
+        system(command);
+        free(filepath);
+        
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }  
 
 int mountFS(char *name) {
@@ -96,5 +160,6 @@ char* getPartitionPath(char *name) {
     char *filepath = (char*) malloc(sizeof(char) * 100);
     strcpy(filepath, partitionPath);
     strcat(filepath, name);
+    strcat(filepath, partitionExt);
     return filepath;
 }
